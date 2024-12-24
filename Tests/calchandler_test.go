faff45030c68a3ca96a2017cfd756aa737b3d804
Ruslan <bust6k.gh@gmail.com>
2024-12-24 17:28:41 +0300

    first commit

---
+++ Tests/calchandler_test.go
@@ -0,0 +1,103 @@
+package Tests
+
+import (
+	"awesomeProject1/CMD"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"math"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func Float64frombytes(bytes []byte) float64 {
+	bits := binary.LittleEndian.Uint64(bytes)
+	float := math.Float64frombits(bits)
+	return float
+}
+func TestCalcHandler(t *testing.T) {
+	TestesSucsess := []struct {
+		expression string
+		excepted   float64
+		wanterr    bool
+	}{
+		{
+			expression: "2+2",
+			wanterr:    false,
+			excepted:   4,
+		},
+		{
+			expression: "2+4*2",
+			wanterr:    false,
+			excepted:   10,
+		},
+		{
+			expression: "2/2",
+			wanterr:    false,
+			excepted:   1,
+		},
+	}
+	for i, _ := range TestesSucsess {
+		expr := TestesSucsess[i].expression
+		requestBody := strings.NewReader(`{"expression":"` + expr + `"}`)
+		req, err := http.NewRequest("POST", "/api/v1/calculate", requestBody)
+		if err != nil {
+			t.Fatal()
+		}
+		w := httptest.NewRecorder()
+		CMD.CalculateHandler(w, req)
+
+		resp := w.Result()
+		defer resp.Body.Close()
+		body, _ := io.ReadAll(resp.Body)
+
+		if Float64frombytes(body) == TestesSucsess[i].excepted {
+			fmt.Println("все правильно")
+		} else {
+			t.Errorf("ожидал %f, а получил %v", TestesSucsess[i].excepted, string(body))
+		}
+	}
+	TestesFail := []struct {
+		expression string
+		excepted   string
+		wanterr    bool
+	}{
+		{
+			expression: "2+2p",
+			wanterr:    true,
+			excepted:   "проверь еще раз твое выражение",
+		},
+		{
+			expression: "2+4*)",
+			wanterr:    true,
+			excepted:   "проверь еще раз твое выражение",
+		},
+		{
+			expression: "",
+			wanterr:    true,
+			excepted:   "проверь еще раз твое выражение",
+		},
+	}
+	for i, _ := range TestesFail {
+		expr := TestesFail[i].expression
+		requestBody := strings.NewReader(`{"expression":"` + expr + `"}`)
+		req, err := http.NewRequest("POST", "/api/v1/calculate", requestBody)
+		if err != nil {
+			t.Fatal()
+		}
+		w1 := httptest.NewRecorder()
+		CMD.CalculateHandler(w1, req)
+
+		resp := w1.Result()
+		defer resp.Body.Close()
+		body1, _ := io.ReadAll(resp.Body)
+
+		if string(body1) == TestesFail[i].excepted {
+			fmt.Println("все правильно")
+		} else {
+			t.Errorf("ожидал %v, а получил %v", TestesFail[i].excepted, string(body1))
+		}
+	}
+}
